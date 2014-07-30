90c559c29bb08e38cbfc96c73d2bc0f22bd896f1
Jay Patel <jaypa@cybage.com>
2014-07-30 11:50:42 +0530

    Removed extended sorting functionality from controller

--- Application-smart-table/example-app/js/app.js
+++ Application-smart-table/example-app/js/app.js
@@ -4,37 +4,13 @@
 angular
 .module('myApp', ['smartTable.table'])
 .controller('mainCtrl', ['$scope', '$http','$timeout', function (scope, http,$timeout) {
-	scope.subHeaders=[[{'label':'subheader1','formatFunction':'uppercase','cellClass':'subheader1'},{'label':'subheader2','formatFunction':'lowercase','cellClass':'subheader2'}]];
+		scope.subHeaders=[[{'label':'subheader1','formatFunction':'uppercase','cellClass':'subheader1'},{'label':'subheader2','formatFunction':'lowercase','cellClass':'subheader2'}]];
         scope.rowCollection = [{'test1':'Test 1','test2':'Test 1 column2'},{'test1':'Test 2 column1','test2':'Test 2'}];
-        scope.columnCollection=[{'label':'Display Column 1','map':'test1'},{'label':'Display Column 2','map':'test2'}]
-        
-    angular.extend(scope, {
-
-        search: function (e, column) {
-
-            console.log(e, column);
-
-        },
-
-        sort: function (e, column) {
-
-            var field = column.map,
-                order = column.reverse ? 'ASC' : 'DESC';
-            
-            console.log(field, order);
-        },
-
-        globalConfig: {
+        scope.columnCollection=[{'label':'Display Column 1','map':'test1','isSortable':true},{'label':'Display Column 2','map':'test2','isSortable':true}];
+         scope.globalConfig= {
             isPaginationEnabled: false,
             isGlobalSearchActivated: true,
-            serverSideFilter: true,
-            serverSideSort: true,
             selectionMode: 'single'
-        }
+        };
 
-    });
-
-    scope.$on('search', scope.search);
-    scope.$on('sortColumn', scope.sort);
-    
 }]);
